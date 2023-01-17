be664869c4732808e0ee8cdc0d24a7cceb09f1c1
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-01-18 01:17:17 +0630

    2.5 done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import {Navbar,Nav,Container} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header className=''>
         <Navbar bg="dark" variant='dark' collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+        <LinkContainer to='/'>
+          <Navbar.Brand>ProShop</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="navbarScroll" className='primary'/>
         <Navbar.Collapse id="navbarScroll" className=''>
           <Nav
             className="flex ml-auto"
             navbarScroll
           >
-            <Nav.Link href="/cart">
+            <LinkContainer to='/cart'>
+            <Nav.Link>
                 <i className='fas fa-shopping-cart px-2'></i>
-                Cart</Nav.Link>
-            <Nav.Link href="/login">
-                <i className='fas fa-user px-2'></i>
-                Sign In
+                Cart
             </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Nav.Link>
+                  <i className='fas fa-user px-2'></i>
+                  Sign In
+              </Nav.Link>
+            </LinkContainer>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
